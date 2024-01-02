356e36d26b9bc1e69d535fdf624d8f451ee9be3d
Ashish <ashishsoni2002@gmail.com>
2024-01-02 10:31:53 +0530

    update api

--- frontend/src/policePortalsRoutes/feedback/FeedbackForm1.js
+++ frontend/src/policePortalsRoutes/feedback/FeedbackForm1.js
@@ -38,7 +38,7 @@ export default function FeedbackForm() {
   const handleSubmit = async () => {
     try {
       // Your backend API endpoint for feedback submission
-      const apiUrl = "YOUR_BACKEND_API_URL_HERE";
+      const apiUrl = "http://localhost:5000/api/feedback/fillFeedback";
 
       // Construct the feedback object to be sent
       const feedbackData = {
