1e3cf172dc6ed95d32817577bd8e30ec4985189e
Ashish <ashishsoni2002@gmail.com>
2024-01-17 09:49:34 +0530

    Revert "added Chatbot"
    This reverts commit 535a60ebeccad80424c684da2f202498324dfae0.

--- Citizen-Webapp-Copy/src/App.js
+++ Citizen-Webapp-Copy/src/App.js
@@ -180,7 +180,8 @@ export default function App() {
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
           />
-      
+          <Configurator />
+          {configsButton}
         </>
       )}
       {layout === "vr" && <Configurator />}
