877638f3a8689a86cd66c820a7c039e49b95c570
Ashish <ashishsoni2002@gmail.com>
2024-03-01 21:37:53 +0530

    Clean Dashboard - Admin

--- Admin-Webapp/src/layouts/dashboard/index.js
+++ Admin-Webapp/src/layouts/dashboard/index.js
@@ -34,6 +34,7 @@ import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
 // Dashboard components
 import Projects from "layouts/dashboard/components/Projects";
 import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
   const { sales, tasks } = reportsLineChartData;
@@ -44,66 +45,75 @@ function Dashboard() {
       <MDBox py={3}>
         <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="dark"
-                icon="weekend"
-                title="Bookings"
-                count={281}
-                percentage={{
-                  color: "success",
-                  amount: "+55%",
-                  label: "than lask week",
-                }}
-              />
-            </MDBox>
+            <Link to="/billing">
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  icon="leaderboard"
+                  title="Feedback Analyser"
+                  // count="2,300"
+                  percentage={{
+                    color: "success",
+                    // amount: "+3%",
+                    label: "Analysing the Feedback",
+                  }}
+                />
+              </MDBox>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={3}>
+            <Link to="/poll">
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  color="success"
+                  icon="store"
+                  title="Poll Manager"
+                  // count=""
+                  percentage={{
+                    color: "success",
+                    // amount: "+1%",
+                    label: "Create / View Polls",
+                  }}
+                />
+              </MDBox>
+            </Link>
           </Grid>
           <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                icon="leaderboard"
-                title="Today's Users"
-                count="2,300"
-                percentage={{
-                  color: "success",
-                  amount: "+3%",
-                  label: "than last month",
-                }}
-              />
-            </MDBox>
+            <Link to="/firr">
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  color="primary"
+                  icon="person_add"
+                  title="FIR Manager"
+                  // count="+91"
+                  percentage={{
+                    color: "success",
+                    amount: "",
+                    label: "Analyse / Create FIRs",
+                  }}
+                />
+              </MDBox>
+            </Link>
           </Grid>
           <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="success"
-                icon="store"
-                title="Revenue"
-                count="34k"
-                percentage={{
-                  color: "success",
-                  amount: "+1%",
-                  label: "than yesterday",
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-              <ComplexStatisticsCard
-                color="primary"
-                icon="person_add"
-                title="Followers"
-                count="+91"
-                percentage={{
-                  color: "success",
-                  amount: "",
-                  label: "Just updated",
-                }}
-              />
-            </MDBox>
+            <Link to="/emailsBulk">
+              <MDBox mb={1.5}>
+                <ComplexStatisticsCard
+                  color="dark"
+                  icon="weekend"
+                  title="Bulk Emails"
+                  // count={281}
+                  percentage={{
+                    color: "success",
+                    amount: "+55%",
+                    label: "Send Emails",
+                  }}
+                />
+              </MDBox>
+            </Link>
           </Grid>
         </Grid>
-        <MDBox mt={4.5}>
+        {/* <MDBox mt={4.5}>
           <Grid container spacing={3}>
             <Grid item xs={12} md={6} lg={4}>
               <MDBox mb={3}>
@@ -143,8 +153,8 @@ function Dashboard() {
               </MDBox>
             </Grid>
           </Grid>
-        </MDBox>
-        <MDBox>
+        </MDBox> */}
+        {/* <MDBox>
           <Grid container spacing={3}>
             <Grid item xs={12} md={6} lg={8}>
               <Projects />
@@ -153,7 +163,7 @@ function Dashboard() {
               <OrdersOverview />
             </Grid>
           </Grid>
-        </MDBox>
+        </MDBox> */}
       </MDBox>
       {/* <Footer /> */}
     </DashboardLayout>
