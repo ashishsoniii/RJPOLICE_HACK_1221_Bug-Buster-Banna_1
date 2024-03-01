15a882a4f4875617e54d106f1660e46a73429a4c
Ashish <ashishsoni2002@gmail.com>
2024-03-01 21:42:02 +0530

    Removing Config Settings!

--- Admin-Webapp/src/App.js
+++ Admin-Webapp/src/App.js
@@ -176,7 +176,7 @@ export default function App() {
             onMouseLeave={handleOnMouseLeave}
           />
           <Configurator />
-          {configsButton}
+          {/* {configsButton} */}
         </>
       )}
       {layout === "vr" && <Configurator />}
